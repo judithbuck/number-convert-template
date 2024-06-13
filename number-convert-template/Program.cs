5a647909ed5d9b7953e336a733c2ef7bdd5fc543
Judith Buck <judith18buckden@hotmail.com>
2024-06-13 20:31:47 +0100

    Add project files.

---
+++ number-convert-template/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace number_convert_template
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input of a denary value
+            Console.WriteLine("");
+
+            //base to convert to as a user input
+
+            //convert the number to the given base
+
+            //output result
+
+        }
+    }
+}
